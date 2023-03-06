35de5b77f6d41d0b76294ae2f9aabe58f070b8b0
niallrox <nia1rox@yandex.ru>
2023-03-06 19:36:44 +0300

    add test endpoint

--- src/main/java/com/djidjya/team/DjidyaJavaKubernetesTemplateApplication.java
+++ src/main/java/com/djidjya/team/DjidyaJavaKubernetesTemplateApplication.java
@@ -3,10 +3,18 @@ package com.djidjya.team;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class DjidyaJavaKubernetesTemplateApplication {
+
+	@GetMapping("/test")
+	public String test() {
+		return "TEST";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DjidyaJavaKubernetesTemplateApplication.class, args);
