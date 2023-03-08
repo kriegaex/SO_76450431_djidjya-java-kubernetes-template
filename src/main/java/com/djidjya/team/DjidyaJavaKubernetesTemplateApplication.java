e95f9c652909cb2b352f93684e8798907dcbd8f3
niallrox <nia1rox@yandex.ru>
2023-03-08 21:09:22 +0300

    add api doc

--- src/main/java/com/djidjya/team/DjidyaJavaKubernetesTemplateApplication.java
+++ src/main/java/com/djidjya/team/DjidyaJavaKubernetesTemplateApplication.java
@@ -1,16 +1,24 @@
 package com.djidjya.team;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@OpenAPIDefinition(info = @Info(title = "Template commands API", version = "1.0", description = "Documentation Template Service API v1.0"))
 @EnableDiscoveryClient
 @SpringBootApplication
 @RestController
 public class DjidyaJavaKubernetesTemplateApplication {
 
+	@Operation(summary = "test")
+	@ApiResponse(responseCode = "200", description = "Created")
+	@ApiResponse(responseCode = "401", description = "Unauthorized")
 	@GetMapping("/test")
 	public String test() {
 		return "TEST";
